04a3fda8adb17ebdaa59c38c2483216e25738c9b
Minhaj Hasan <hasaniiuc0@gmail.com>
2026-08-10 11:54:10 +0600

    Class 06 Tweet.cs class done

--- TwitterClone/TwitterClone.Domain/Entities/Tweet.cs
+++ TwitterClone/TwitterClone.Domain/Entities/Tweet.cs
@@ -1,12 +1,38 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿
 
 namespace TwitterClone.Domain.Entities
 {
-    internal class Tweet
+    public class Tweet
     {
+        private Guid _tweetId;
+        private Guid _authorId;
+        private DateTime _tweetedAt;
+        private DateTime? _modifyedAt;
+        private string _content;
+
+
+
+        public Guid Id
+        {
+            get { return _tweetId; }
+        }
+
+        public Guid AuthorId
+        {
+            get { return _authorId; }
+        }
+
+        public DateTime TweetedAt
+        {
+            get { return _tweetedAt; }
+            set
+            {
+                _tweetedAt = DateTime.UtcNow;
+            }
+        }
+
+
+        public  Tweet()
+
     }
 }
